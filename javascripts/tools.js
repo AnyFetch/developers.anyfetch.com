5f36bb5b7f2d7714e400b042007c4420da9d606e
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-05-21 17:35:01 +0200

    fix margin top when scroll

--- javascripts/tools.js
+++ javascripts/tools.js
@@ -8,7 +8,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 100
         }, 300);
         return false;
       }
