d98b4ada7469fd83cd3dfa25edc049664aad4336
buathi_q <buathi_q@epitech.eu>
2014-07-04 17:52:05 +0200

    Remove useless comments

--- bin/updateDocumentTypes.js
+++ bin/updateDocumentTypes.js
@@ -23,22 +23,6 @@ var ids = [
   '5252ce4ce4cfcd16f55cfa41'
 ];
 
-/*var fileBegin = "---\n\
-title: AnyFetch document-types resources \n\
-layout: doc \n\
---- \n\
-{% raw  %} \n\
- \n\
-You'll find here the list of default document-types and their expectations. \n\
-Don't forget that [/resources/hydraters.html](many hydraters can change the `document_type` during the hydration phase). \n\
- \n\
-If you're not satisfied with the default presentation or you find some data is missing: \n\
- \n\
-* You can always add more `metadata`. Even though it won't be shown to the user, the document will still be returned to the user for a relevant query, as all metadata keys are used in search. \n\
-* You can create your own document type and make a simple hydrater updating the 'old' document-type to the new one. \n\n";
-
-var fileEnd = "{% endraw %}";*/
-
 var content = "";
 
 var idsFunctions = [];
