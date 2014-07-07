2a7fd52529f914b2af7bfc4379229cd181732d83
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-07-07 16:00:23 +0200

    set hash, bug not fixed...

--- javascripts/tools.js
+++ javascripts/tools.js
@@ -3,11 +3,14 @@ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      var hash = this.hash;
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top - 100
-        }, 1000);
+        }, 1000, function() {
+            location.hash = hash;
+        });
         return false;
       }
     }
